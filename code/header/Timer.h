d126fb0e2823dd548c49e810a020110ffa7494f7
Xinlly <Xinlly@outlook.com>
2023-06-20 20:49:37 +0800

    系统时钟参数使用宏定义

--- code/header/Timer.h
+++ code/header/Timer.h
@@ -2,7 +2,13 @@
 #define _Timer_h
 
 //系统时钟周期（us）
-#define sysTimerCycle 500
+#define sysTimer_Cycle 500
+// 设置定时器模式01_500us@11.0592MHz
+#define sysTimer_mod 0x01
+// FE for 500us@11.0592MHz
+#define sysTimer_initValue_H 0xFE
+//33 for 500us@11.0592MHz，考虑LED显示，矫正为53
+#define sysTimer_initValue_L 0x53
 
 extern bit flag_inter;
 
--- code/source/Timer.c
+++ code/source/Timer.c
@@ -1,27 +1,28 @@
 #include "REG52.H"
 #include "CustomType.h"
 #include "Led.h"
+#include "Timer.h"
 uint32 currentSysTime_us = 0;
 uint32 currentSysPeriod = 0;
 uint32 taskPlan_timeMarkMs;
 bit flag_inter = 0;
 void interruptTimer0() interrupt 1
 {
-	TL0 = 0x53;
-	TH0 = 0xFE;
+	TL0 = sysTimer_initValue_L;
+	TH0 = sysTimer_initValue_H;
 	currentSysPeriod += 1;
-	currentSysTime_us += 500;
+	currentSysTime_us += sysTimer_Cycle;
 	ledDisplayTask();
 }
 void initTimer0()
 {
-	TMOD |= 0x01; // 设置定时器模式01_500us@11.0592MHz
-	TL0 = 0x53;	  // 设置定时初始值TL33_500us@11.0592MHz，考虑LED显示矫正为53
-	TH0 = 0xFE;	  // 设置定时初始值THFE_500us@11.0592MHz
-	TF0 = 0;	  // 清除TF0标志
+	TMOD |= sysTimer_mod;
+	TL0 = sysTimer_initValue_L;	  
+	TH0 = sysTimer_initValue_H;	  
+	TF0 = 0;	  
 	EA = 1;
 	ET0 = 1;
-	TR0 = 1; // 定时器0开始计时
+	TR0 = 1; 
 }
 void initTimer1() // 500us@11.0592MHz
 {
@@ -29,7 +30,7 @@ void initTimer1() // 500us@11.0592MHz
 	TL1 = 0x00;	  
 	TH1 = 0x00;	  
 	TF1 = 0;
-	P3 = 0xFF;
+	T1 = 0xFF;
 }
 uint32 getSysPeriod()
 {
